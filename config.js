dc0e8336a088936852c67892257906d4046d36ef
Brandon Jones <bdjones2410@gmail.com>
2016-05-05 00:11:50 -0400

    updated config file mongodb uri

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGO_URI: process.env.MONGOLAB_URI || process.env.MONGO_URI || 'mongodb://localhost:27017/MEAN_TODO_DB',
+  MONGO_URI: process.env.MONGOLAB_URI || process.env.MONGODB_URI || 'mongodb://localhost:27017/MEAN_TODO_DB',
   TOKEN_SECRET: process.env.TOKEN_SECRET || 'Dragons taste like squirrel meat'
 };
 
